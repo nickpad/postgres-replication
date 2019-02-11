6f189a979eb364f7f6b23bd10182d154a3961c30
Nick Dainty <nickdainty@gmail.com>
2019-02-11 11:58:41 +0000

    Don't use replication slots
    I now understand that using replication slots will keep WAL files on the
    master indefinitely if the replica falls behind. This doesn't seem like
    a good default config as it will eat disk space on the master.
    
    There's lots of good info in this article:
    
    https://www.opsdash.com/blog/postgresql-replication-slots.html

--- replica/start_replication.sh
+++ replica/start_replication.sh
@@ -12,7 +12,7 @@ wait-for-it master:5432
 #
 # Note that the PG environment variables set in the docker-compose file are used to
 # connect to the master.
-pg_basebackup --host=master --pgdata=/var/lib/postgresql/data --write-recovery-conf --create-slot --slot=$REP_SLOT --verbose --progress
+pg_basebackup --host=master --pgdata=/var/lib/postgresql/data --write-recovery-conf --progress
 
 # Once the base backup has been taken, postgres can be started. Because we're running
 # in a Docker container, the "docker-entrypoint.sh" script that's provided by the
