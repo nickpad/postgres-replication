83dc4b98d68354c415d03e2e919dd98580763a19
Nick Dainty <nickdainty@gmail.com>
2019-02-11 11:16:40 +0000

    Extract credentials into environment vars

--- master/setup.sh
+++ master/setup.sh
@@ -21,12 +21,12 @@ EOF
 
 # Create a replication role with login permission.
 # These credentials will be used to connect to the master from the replica instance.
-psql --username "$POSTGRES_USER" --command "CREATE ROLE splice_rep WITH REPLICATION LOGIN ENCRYPTED PASSWORD 'test';"
+psql --username "$POSTGRES_USER" --command "CREATE ROLE $REP_USER WITH REPLICATION LOGIN ENCRYPTED PASSWORD '$REP_PASSWORD';"
 
 # The pg_hba.conf file controls access to the PG server. This script
 # writes a config file that allows the "splice_rep" username to open
 # a replication connection from any host as long as it supplies the
 # correct password.
 cat <<EOF >> "/var/lib/postgresql/data/pg_hba.conf"
-hostssl replication splice_rep 0.0.0.0/0 md5
+hostssl replication $REP_USER 0.0.0.0/0 md5
 EOF
--- replica/start_replication.sh
+++ replica/start_replication.sh
@@ -6,17 +6,13 @@ set -e
 # on port 5432.
 wait-for-it master:5432
 
-# These environment variables will be used to authenticate when connecting to the master.
-export PGUSER="splice_rep"
-export PGPASSWORD="test"
-export PGSSLMODE="require"
-export PGAPPNAME="splice"
-export PGHOST="master"
-
 # Before streaming replication can start, the replica needs to take a base backup of the
 # contents of the master. To do this, "pg_basebackup" is used. See the pg_basebackup man
 # page for a full explanation of the options that have been used here.
-pg_basebackup --pgdata=/var/lib/postgresql/data --write-recovery-conf --create-slot --slot=splice --verbose --progress
+#
+# Note that the PG environment variables set in the docker-compose file are used to
+# connect to the master.
+pg_basebackup --host=master --pgdata=/var/lib/postgresql/data --write-recovery-conf --create-slot --slot=$REP_SLOT --verbose --progress
 
 # Once the base backup has been taken, postgres can be started. Because we're running
 # in a Docker container, the "docker-entrypoint.sh" script that's provided by the
